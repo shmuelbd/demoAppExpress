f269debeab015a210e464780283a45ad3f618406
shmuelbd <shmuelbd1@gmail.com>
2022-11-26 22:04:22 +0200

    initial 1

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.post("/webhook", (req, res) => {
     let body = req.body;
 
     // Check the Incoming webhook message
-    console.log("Incoming", body);
+    console.log("Incoming ", body);
 
     // info on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
     if (req.body.object) {
